d12a6b3201d782f1ce6ee01914330823fe2db966
imRB7here <imRB7here@gmail.com>
2019-12-15 21:40:32 +0530

    minor change

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.uic import *
+from Calculator import Calculator
 
 app=QApplication(sys.argv)
-
+calculator=Calculator(title='Calculator',parent=None,uiFile='form/ui_calculator.ui')
 app.exec_()
